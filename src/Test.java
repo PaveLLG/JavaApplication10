0c65279356d9695a76c405eed4d35cdbb1c0264a
PaveL <BPaveL07@yandex.ru>
2021-08-27 13:31:08 +0300

    Add tset

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+
+    private int x;
+
+    public Test(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" + "x=" + x + '}';
+    }
+}
